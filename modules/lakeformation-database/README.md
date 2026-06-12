0593b7176d00921a3cc2ec58196d3da1ead3235a
zerj9 <96551236+zerj9@users.noreply.github.com>
2026-06-12 10:16:28 +0100

    initial lf database module for internal data factory use

---
+++ modules/lakeformation-database/README.md
@@ -0,0 +1,58 @@
+<!-- BEGIN_TF_DOCS -->
+# Lake Formation Database
+This module creates an S3 location registered in AWS Lake Formation and an AWS Glue Catalog Database backed by that S3 bucket.
+
+## Usage
+```hcl
+module "lakeformation_database" {
+  source = "github.com/ministryofjustice/terraform-aws-moj-data-factory-modules//modules/lakeformation-database?ref=<git-sha>"
+
+  database_name = "example"
+
+  storage = {
+    bucket_name = <bucket_name>
+    prefix      = "example"
+    kms_key_arn = "arn:aws:kms:eu-west-2:1234567890:key/example"
+  }
+  tags          = local.tags
+}
+```
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.5 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | ~> 6.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | ~> 6.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_glue_catalog_database.data_factory_database](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/glue_catalog_database) | resource |
+| [aws_iam_role.lakeformation_s3_access_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.lakeformation_s3_access_policy](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_lakeformation_resource.s3_location](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lakeformation_resource) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_database_name"></a> [database\_name](#input\_database\_name) | The name of the Glue catalog database to create. | `string` | n/a | yes |
+| <a name="input_storage"></a> [storage](#input\_storage) | S3 location backing the Glue database (bucket name, key prefix, and KMS key ARN). | <pre>object({<br/>    bucket_name = string<br/>    prefix      = string<br/>    kms_key_arn = string<br/>  })</pre> | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | Tags to apply to created resources. | `map(string)` | `{}` | no |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ modules/lakeformation-database/main.tf
@@ -0,0 +1,98 @@
+/**
+  * # Lake Formation Database
+  * This module creates an S3 location registered in AWS Lake Formation and an AWS Glue Catalog Database backed by that S3 bucket.
+  *
+  * ## Usage
+  * ```hcl
+  * module "lakeformation_database" {
+  *   source = "github.com/ministryofjustice/terraform-aws-moj-data-factory-modules//modules/lakeformation-database?ref=<git-sha>"
+  *
+  *   database_name = "example"
+  *
+  *   storage = {
+  *     bucket_name = <bucket_name>
+  *     prefix      = "example"
+  *     kms_key_arn = "arn:aws:kms:eu-west-2:1234567890:key/example"
+  *   }
+  *   tags          = local.tags
+  * }
+  * ```
+*/
+
+resource "aws_iam_role" "lakeformation_s3_access_role" {
+  name = "${var.database_name}-s3-access"
+  tags = var.tags
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lakeformation.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lakeformation_s3_access_policy" {
+  name = "lakeformation-s3-access-policy"
+  role = aws_iam_role.lakeformation_s3_access_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ListBucket"]
+        Resource = ["arn:aws:s3:::${var.storage.bucket_name}"]
+        Condition = {
+          StringLike = {
+            "s3:prefix" = [
+              trim(var.storage.prefix, "/"),
+              "${trim(var.storage.prefix, "/")}/",
+              "${trim(var.storage.prefix, "/")}/*"
+            ]
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = ["arn:aws:s3:::${var.storage.bucket_name}/${trim(var.storage.prefix, "/")}/*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = [var.storage.kms_key_arn]
+      }
+    ]
+  })
+}
+
+resource "aws_lakeformation_resource" "s3_location" {
+  role_arn   = aws_iam_role.lakeformation_s3_access_role.arn
+  arn        = "arn:aws:s3:::${var.storage.bucket_name}/${trim(var.storage.prefix, "/")}"
+  depends_on = [aws_iam_role_policy.lakeformation_s3_access_policy]
+}
+
+resource "aws_glue_catalog_database" "data_factory_database" {
+  name         = var.database_name
+  location_uri = "s3://${var.storage.bucket_name}/${var.storage.prefix}"
+  description  = "Glue catalog database for ${var.database_name}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "data-factory-${var.database_name}-glue-database"
+    }
+  )
+}
