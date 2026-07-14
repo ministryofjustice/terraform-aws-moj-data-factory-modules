b4e74f8eb8ef86b9d64116188e2e38f25bbec120
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-07-14 11:05:41 +0100

    Wording change in outputs

--- modules/s3-raw-bucket/outputs.tf
+++ modules/s3-raw-bucket/outputs.tf
@@ -1,13 +1,13 @@
 
 # Returns the name (ID) of the S3 bucket created by this module.
 output "bucket_name" {
-  description = "Name of the scanned S3 bucket."
+  description = "Name of the S3 bucket."
   value       = module.bucket.s3_bucket_id
 }
 
 # Returns the Amazon Resource Name (ARN) of the bucket.
 output "bucket_arn" {
-  description = "ARN of the scanned S3 bucket."
+  description = "ARN of the S3 bucket."
   value       = module.bucket.s3_bucket_arn
 }
 
