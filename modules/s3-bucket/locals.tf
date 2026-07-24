9766df160c4d143612603032625856d31a928fdb
moj-supratikc <supratik.chowdhury@justice.gov.uk>
2026-07-24 13:42:43 +0100

    docs

--- modules/s3-bucket/locals.tf
+++ modules/s3-bucket/locals.tf
@@ -1,14 +1,14 @@
-locals {
-  aws_account_id = data.aws_caller_identity.current.account_id
+locals { 
 
-  bucket_name = "${var.bucket_prefix}-${local.aws_account_id}-${data.aws_region.current.region}-an"
+  bucket_name = "${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an"
 
   common_tags = merge(
     var.tags,
     {
       ManagedBy = "Terraform"
       Region    = data.aws_region.current.region
-      Account   = local.aws_account_id
+      AccountId   = data.aws_caller_identity.current.account_id
+      AccountName = terraform.workspace
     }
   )
 }
