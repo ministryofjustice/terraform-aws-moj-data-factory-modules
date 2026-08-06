1fda58d5dd748dd2ee00f9f8a2ee6617e20afadb
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-08-06 17:03:44 +0100

    outputs fix

--- modules/external-i-am-role/outputs.tf
+++ modules/external-i-am-role/outputs.tf
@@ -7,8 +7,3 @@ output role_name {
   description = "Name of the IAM role."
   value       = aws_iam_role.this.name
 }  
-
-output policy_arn {
-  description = "ARN of the IAM policy."
-  value       = aws_iam_policy.this.arn
-}
