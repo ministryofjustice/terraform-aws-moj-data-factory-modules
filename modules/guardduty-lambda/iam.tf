e564716954cd0dfc34af32b7b5cde382e588aea0
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-08-06 15:30:38 +0100

    initial dev of lambda to move failed scan objects

---
+++ modules/guardduty-lambda/iam.tf
@@ -0,0 +1,64 @@
+# IAM role for the quarantine Lambda function.
+# This role allows the Lambda function to read from the scanned bucket, write to the quarantine bucket, and log to CloudWatch.
+
+resource "aws_iam_role" "quarantine_lambda" {
+  name = "${var.name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject"
+        ]
+        Resource = ["${var.quarantine_bucket_arn}/*"]
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = ["${var.s3_bucket_arn}/*"]
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = [var.s3_bucket_kms_key_arn]
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+        "kms:Decrypt",
+        "kms:Encrypt",
+        "kms:GenerateDataKey"
+        ]
+        Resource = [var.quarantine_kms_key_arn]
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+# Permission to write logs to CloudWatch.
+resource "aws_iam_role_policy_attachment" "logs" {
+  role       = aws_iam_role.quarantine_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
