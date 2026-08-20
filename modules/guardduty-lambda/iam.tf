2f6183f6c8f278bb97d25eb63e379cdce342f4f4
benjamin-finucane <86959450+benjamin-finucane@users.noreply.github.com>
2026-08-20 15:50:48 +0100

    fixed malformed policy doc

--- modules/guardduty-lambda/iam.tf
+++ modules/guardduty-lambda/iam.tf
@@ -1,60 +1,74 @@
 # IAM role for the quarantine Lambda function.
 # This role allows the Lambda function to read from the scanned bucket, write to the quarantine bucket, and log to CloudWatch.
 
+
 resource "aws_iam_role" "quarantine_lambda" {
   name = "${var.name}-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Effect = "Allow"
+
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-        Action = "sts:AssumeRole"
-      },
 
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "quarantine_lambda" {
+  name = "${var.name}-policy"
+  role = aws_iam_role.quarantine_lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
       {
         Effect = "Allow"
+
         Action = [
           "s3:PutObject"
         ]
-        Resource = ["${var.quarantine_bucket_arn}/*"]
-      },
 
+        Resource = [
+          "${var.quarantine_bucket_arn}/*"
+        ]
+      },
       {
         Effect = "Allow"
+
         Action = [
           "s3:GetObject",
           "s3:DeleteObject"
         ]
-        Resource = ["${var.s3_bucket_arn}/*"]
-      },
 
+        Resource = [
+          "${var.s3_bucket_arn}/*"
+        ]
+      },
       {
         Effect = "Allow"
+
         Action = [
           "kms:Decrypt",
           "kms:Encrypt",
           "kms:GenerateDataKey"
         ]
-        Resource = [var.s3_bucket_kms_key_arn]
-      },
 
-      {
-        Effect = "Allow"
-        Action = [
-        "kms:Decrypt",
-        "kms:Encrypt",
-        "kms:GenerateDataKey"
+        Resource = [
+          var.s3_bucket_kms_key_arn,
+          var.quarantine_kms_key_arn
         ]
-        Resource = [var.quarantine_kms_key_arn]
       }
     ]
   })
-
-
 }
 
 # Permission to write logs to CloudWatch.
